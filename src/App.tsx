8a5cae38f4976a6f1cd5a0d1ae91b64891147fdc
Andy Waine <88730354+Andy-Waine@users.noreply.github.com>
2023-05-04 16:55:40 -0400

    initial react app, minus unnecessary files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-root">
+      <h1>Howdy, Partner!</h1>
+    </div>
+  );
+}
+
+export default App;
