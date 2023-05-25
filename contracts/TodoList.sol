8db45151ec574f8ed2aa341a68ae37f9a2d0bd31
Andy Waine <88730354+Andy-Waine@users.noreply.github.com>
2023-05-25 17:36:51 -0400

    removing test contract and its corresponding build

--- contracts/TodoList.sol
+++
@@ -1,8 +0,0 @@
-// NOTE: this is just a test contract to ensure truffle and ganache are working properly
-
-pragma solidity ^0.8.19;
-
-
-contract TodoList {
-    string public printTaskCount = "1 task(s)";
-}
